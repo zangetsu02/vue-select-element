bd44d1f2aaa5d069e556777f6693395d86937ce0
Christian Palladini <christian.palladini2@gmail.com>
2023-09-01 02:25:35 +0200

    Configurato il vite.config nella modalita lib, aggiunte dipendeze dev, aggiunto componente

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      // src/index.ts is where we have exported the component(s)
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "SelectElement",
+      // the name of the output files when the build is run
+      fileName: "select-element-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
