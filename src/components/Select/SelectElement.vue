532886d277251e61915c72d94e5c265bd7b68421
Christian Palladini <christian.palladini2@gmail.com>
2023-09-01 02:52:32 +0200

    WIP: Iniziato il componente, stesura template di base

--- src/components/Select/SelectElement.vue
+++ src/components/Select/SelectElement.vue
@@ -1,8 +1,22 @@
 <template>
-
+    <div
+        class="select-element"
+        @click="openMenu"
+    >
+        <input type="text">
+        <div v-if="isOpen">OPEN</div>
+    </div>
 </template>
 
 <script setup lang="ts">
+
+import {ref} from "vue";
+
+const isOpen = ref<Boolean>(false)
+
+function openMenu() {
+    isOpen.value = !isOpen.value
+}
 
 </script>
 
