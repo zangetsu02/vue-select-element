8f383424c68facabd7a982694f39e923844d55f5
Christian Palladini <christian.palladini2@gmail.com>
2023-09-04 01:18:47 +0200

    Aggiunto v-model per l'input e implementata logica per renderizzare il label dell'option selezionata

--- src/components/Select/children/SelectInput.vue
+++ src/components/Select/children/SelectInput.vue
@@ -1,9 +1,27 @@
 <template>
-    <input class="select-input bg-white default-border" type="text">
+    <input
+        class="select-input bg-white default-border"
+        type="text"
+        v-model="modelValue"
+    >
 </template>
 
 <script setup lang="ts">
 
+import {computed, watch} from "vue";
+
+const props = defineProps<{
+    displayValue: string
+}>()
+
+const modelValue = defineModel<string>()
+const displayValue = computed(() => {
+    return props.displayValue
+})
+
+watch(displayValue, () => {
+    modelValue.value = props.displayValue
+})
 </script>
 
 <style scoped>
