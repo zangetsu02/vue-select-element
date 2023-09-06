d626a7980b2362df623d60d87f85e7b8a93b438f
Christian <christian.palladini@linksmt.it>
2023-09-06 23:56:04 +0200

    wip

---
+++ src/components/Combobox/BaseCombobox.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="combobox" v-on-click-outside="onClickOutside">
+    <slot>
+        <ComboboxButton/>
+    </slot> 
+</div>
+
+</template>
+
+<script setup lang="ts">
+import vOnClickOutside from "../../utils/utils.ts";
+import ComboboxButton from "./Children/ComboboxButton"
+
+const props = defineProps<{
+    onClickOutside: Function
+}>()
+</script>
---
+++ src/components/Combobox/Children/ComboboxButton.vue
@@ -0,0 +1,16 @@
+<template>
+
+<button class="combobox-button">
+    <slot>
+        
+    </slot> 
+</button>
+
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    onClickOutside: Function
+}>()
+</script>
