571cf1c687bbf5fe404b74c25c4393cf463d27fc
Christian <christian.palladini2@gmail.com>
2023-11-12 17:03:57 +0100

    bug fix typescript error

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,14 +1,14 @@
 
 const vOnClickOutside = {
-    mounted(el, binding) {
-        el.clickOutsideEvent = (e) => {
+    mounted(el: any, binding: any) {
+        el.clickOutsideEvent = (e: any) => {
             if (!(el === e.target || el.contains(e.target))) {
                 binding.value();
             }
         };
         document.body.addEventListener('click', el.clickOutsideEvent);
     },
-    unmounted(el) {
+    unmounted(el: any) {
         document.body.removeEventListener('click', el.clickOutsideEvent);
     },
 };
